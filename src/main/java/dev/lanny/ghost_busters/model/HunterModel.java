ab6bc27f15a338c354a9f5183ca2477d7c0b1ead
LannyRivero <lannyrivero288@gmail.com>
2025-02-07 13:33:23 +0100

    feat: Implement meethod CaptureGhost. Test ok

--- src/main/java/dev/lanny/ghost_busters/model/HunterModel.java
+++ src/main/java/dev/lanny/ghost_busters/model/HunterModel.java
@@ -1,23 +1,30 @@
 package dev.lanny.ghost_busters.model;
-
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class HunterModel {
 
-    public HunterModel(String string) {
-        //TODO Auto-generated constructor stub
+public class HunterModel {
+    private String name;
+    private List<GhostModel> capturedGhosts;
+
+    public HunterModel(String name) {
+        this.name = name;
+        this.capturedGhosts = new ArrayList<>();
     }
 
-    public void captureGhost(GhostModel ghost1) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'captureGhost'");
+    public void captureGhost(GhostModel ghost) {
+        if (ghost != null) {
+            capturedGhosts.add(ghost);
+            System.out.println("Fantasma capturado: " + ghost.getName());
+        }
     }
 
     public List<GhostModel> getAllGhosts() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAllGhosts'");
+        return new ArrayList<>(capturedGhosts);
     }
- 
+
+
+    public String getName() {
+        return name;
+    }
 }
