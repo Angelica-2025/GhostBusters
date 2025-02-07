b69463b4504a47da0d1b4baa68e033b226a484b5
LannyRivero <lannyrivero288@gmail.com>
2025-02-07 22:25:20 +0100

    feat: Implement method for class ListGhostView ,test passed successfully

--- src/main/java/dev/lanny/ghost_busters/view/ListGhostsView.java
+++ src/main/java/dev/lanny/ghost_busters/view/ListGhostsView.java
@@ -4,15 +4,12 @@ import dev.lanny.ghost_busters.controller.HunterController;
 import dev.lanny.ghost_busters.model.GhostModel;
 import java.util.List;
 
-public class ListGhostsView {
+public class ListGhostsView {  
 
-   
-
-    public static void displayCapturedGhosts(HunterController hunterController) {
-        // Obtener la lista de fantasmas capturados desde el controlador
+    public static void displayCapturedGhosts(HunterController hunterController) {   
         List<GhostModel> ghosts = hunterController.getCapturedGhosts();
 
-        // Mostrar los fantasmas capturados o el mensaje si no hay ninguno
+      
         System.out.println("============================================");
         System.out.println("      Ver Lista de Fantasmas Capturados     ");
         System.out.println("============================================");
@@ -20,11 +17,11 @@ public class ListGhostsView {
         if (ghosts.isEmpty()) {
             System.out.println("❌ No hay fantasmas capturados.");
         } else {
-            // Ajustando el espacio entre las columnas (20 caracteres para el nombre, 15 para la clase, etc.)
+           
             System.out.printf("%-5s %-20s %-15s %-12s %-12s%n", "ID", "Nombre", "Clase", "Peligro", "Fecha");
             System.out.println("------------------------------------------------------------");
             for (GhostModel ghost : ghosts) {
-                // Ajustando el espacio entre las columnas
+              
                 System.out.printf("%-5d %-20s %-15s %-12s %-12s%n",
                         ghost.getId(),
                         ghost.getName(),
@@ -37,6 +34,7 @@ public class ListGhostsView {
         }
     }
 }
+
 
 
 
